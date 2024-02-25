c047a7cf58d1a2c8f213baa972301671c12307f3
Khikmatullin Bulat <colonspawn@gmail.com>
2024-02-25 13:01:36 +0800

    final fixes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import fs from "fs";
+import { fileURLToPath } from 'url';
+import { readFileSync } from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
+import buildDiff from "./buildDiff.js";
+import getFormatting from "./Formatters/index.js";
+import parse from "./parsers.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const getPath = (filename) => path.resolve(__dirname, "..", "__fixtures__", filename);
 
-const getPath = (filename) =>
-  path.resolve(__dirname, "..", "__fixtures__", filename);
-const readFile = (filename) => fs.readFileSync(getPath(filename), "utf-8");
+const getFileFormat = (filename) => path.extname(filename).slice(1);
 
-const genDiff = (filepath1, filepath2) => {
-  const read1 = readFile(filepath1);
-  const read2 = readFile(filepath2);
-  const fileObj1 = JSON.parse(read1);
-  const fileObj2 = JSON.parse(read2);
-  return [fileObj1, fileObj2];
+const readFile = (filepath) => readFileSync(filepath, "utf8");
+
+const parser = (filepath1, filepath2, formatName = "stylish") => {
+  const path1 = getPath(filepath1);
+  const data1 = parse(readFile(path1), getFileFormat(filepath1));
+
+  const path2 = getPath(filepath2);
+  const data2 = parse(readFile(path2), getFileFormat(filepath2));
+
+  const diff = buildDiff(data1, data2);
+  const formattedDiff = getFormatting(diff, formatName);
+  return formattedDiff;
 };
 
-export default genDiff;
+export default parser;
